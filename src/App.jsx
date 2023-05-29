5aeade022a43ae8168298c50a2f1715e9dcc1793
Eshaan Tanzim Sabur <eshaansabur12@gmail.com>
2023-05-29 21:00:36 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Problem1 from "./components/Problem-1.jsx";
 import Menu from "./components/Menu.jsx";
 import Problem2 from "./components/Problem-2.jsx";
 import Index from "./components/Index.jsx";
+import All from './components/All';
+import Active from './components/Active';
+import Complete from './components/Complete';
 
 function App() {
   return (
