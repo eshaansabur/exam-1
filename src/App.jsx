01bacb0933d5f8c1b829a5a5637e60a42d5c6b0a
Eshaan Tanzim Sabur <eshaansabur12@gmail.com>
2023-05-29 20:57:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Problem1 from "./components/Problem-1.jsx";
+import Menu from "./components/Menu.jsx";
+import Problem2 from "./components/Problem-2.jsx";
+import Index from "./components/Index.jsx";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Menu />}>
+          <Route path="problem-1" element={<Problem1 />} />
+          <Route path="problem-2" element={<Problem2 />} />
+          <Route path='/all' element={<All></All>}></Route>
+          <Route path='/active' element={<Active></Active>}></Route>
+          <Route path='/completed' element={<Complete></Complete>}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
